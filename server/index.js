5193ad7a60650cbb2da0d63b0c6e5c52b1289d20
Dylan Foster <dylan947@gmail.com>
2016-01-24 03:11:20 -0800

    feat(server): fix app boot in main script

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 "use strict";
 
+const http = require("http");
+
 const App = require("./lib/app");
-const Router = require("./lib/router");
 
-let app = new App();
+App.boot().then(app => {
+  let server = http.createServer(app);
 
-app.boot(function () {
-  console.log(app.app.get("port"));
-});
+  server.listen(app.get("port"), function () {
+    console.log(`Bonfire listening on 0.0.0.0:${app.get("port")}`);
+  });
+}).catch(console.err);

da99f3843168922325cd32271b47110cf4a7078d
Dylan Foster <dylan947@gmail.com>
2016-04-23 18:20:26 -0700

    feat(templates): add loading to posts and themes routes

--- admin/app/routes/pages/index.js
+++ admin/app/routes/pages/index.js
@@ -2,7 +2,6 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   model() {
-    // this.transitionTo("pages.index_loading");
     return this.store.query("post", { type: "page" });
   },
 
--- admin/app/routes/posts/index.js
+++ admin/app/routes/posts/index.js
@@ -2,7 +2,7 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll("post");
+    return this.store.query("post", { type: "post" });
   },
 
   setupController(controller, model) {

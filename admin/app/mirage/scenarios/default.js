e40afdf0da30de0ee8dbfcdc20807e6af179f0d2
Dylan Foster <dylan947@gmail.com>
2016-04-24 23:50:47 -0700

    feat(page-filter): add search box and icon

--- admin/app/mirage/scenarios/default.js
+++ admin/app/mirage/scenarios/default.js
@@ -1,15 +1,36 @@
 export default function(server) {
+  //pages
+  server.create("post", {
+    name: "First page",
+    content: "<h1>This is my first page</h1>",
+    type: "page",
+    status: "draft"
+  });
+
+  server.create("post", {
+    name: "Second page",
+    content: "<h1>This is my second page</h1>",
+    type: "page",
+    status: "published"
+  });
+
+  // posts
   server.create("post", {
     name: "First post",
     content: "<h1>This is my first post</h1>",
     type:"post",
     status: "published"
   });
-
   server.create("post", {
-    name: "First page",
-    content: "<h1>This is my first page</h1>",
-    type: "page",
+    name: "Second post",
+    content: "<h1>This is my second post</h1>",
+    type:"post",
+    status: "published"
+  });
+  server.create("post", {
+    name: "Third post",
+    content: "<h1>This is my second post</h1>",
+    type:"post",
     status: "draft"
   });
 }

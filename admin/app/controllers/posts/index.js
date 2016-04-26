11965a5a19839f83dd8bf68e3901ce4b39c83b05
Dylan Foster <dylan947@gmail.com>
2016-04-25 22:15:55 -0700

    feat(page-filter): wire up search filter for posts

--- admin/app/controllers/posts/index.js
+++ admin/app/controllers/posts/index.js
@@ -3,15 +3,25 @@ import Ember from "ember";
 export default Ember.Controller.extend({
   application: Ember.inject.controller(),
   globalActions: Ember.inject.service(),
-
-  filteredPosts: Ember.computed("status", "posts", function () {
-    const posts = this.get("posts");
-    const status = this.get("status");
-
-    return posts.filterBy("status", status);
-  }),
   queryParams: ["status"],
+  searchText: null,
   status: "published",
+
+  filteredPosts: Ember.computed("posts", "searchText", "status", function () {
+    const posts = this.get("posts");
+    const searchText = this.get("searchText");
+    const status = this.get("status");
+    let filteredPosts = posts.filterBy("status", status);
+
+    if (searchText) {
+      const regex = new RegExp(searchText, "i");
+
+      filteredPosts = filteredPosts.filter(post => post.get("name").match(regex));
+    }
+
+    return filteredPosts;
+  }),
+
 
   actions: {
     updateFilter(type) {

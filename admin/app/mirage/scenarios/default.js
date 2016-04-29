633ba2b953de27f3bd0364674d80764497b04f4f
Dylan Foster <dylan947@gmail.com>
2016-04-29 01:03:34 -0700

    feat(styles): adjust page content and menu

--- admin/app/mirage/scenarios/default.js
+++ admin/app/mirage/scenarios/default.js
@@ -1,17 +1,45 @@
 export default function (server) {
   // pages
   server.create("post", {
-    name: "First page",
-    content: "<h1>This is my first page</h1>",
+    name: "About",
+    content: "<h1>About</h1>",
     type: "page",
     status: "draft"
   });
 
   server.create("post", {
-    name: "Second page",
-    content: "<h1>This is my second page</h1>",
+    name: "Contact",
+    content: "<h1>Contact</h1>",
     type: "page",
     status: "published"
+  });
+
+  server.create("post", {
+    name: "Home",
+    content: "<h1>Bonfire</h1>",
+    type: "page",
+    status: "published"
+  });
+
+  server.create("post", {
+    name: "FAQ",
+    content: "<h1>FAQ</h1>",
+    type: "page",
+    status: "published"
+  });
+
+  server.create("post", {
+    name: "Privay policy",
+    content: "<h1>Privacy policy</h1>",
+    type: "page",
+    status: "published"
+  });
+
+  server.create("post", {
+    name: "Products",
+    content: "<h1>Products</h1>",
+    type: "page",
+    status: "draft"
   });
 
   // posts
